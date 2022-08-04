f06d4775a8363fbd858bfd5a7c8aca00db283178
Tom Larsen <larsenthomasj@gmail.com>
2022-08-04 15:52:18 -0400

    port lzf

---
+++ src/yxdb/lzf.py
@@ -0,0 +1,64 @@
+class Lzf:
+    def __init__(self, in_bytes: bytearray, out_bytes: bytearray):
+        self.in_bytes = in_bytes
+        self.out_bytes = out_bytes
+        self.iidx = 0
+        self.oidx = 0
+        self.in_len = 0
+
+    def decompress(self, length: int) -> int:
+        self.in_len = length
+        self._reset()
+
+        if self.in_len == 0:
+            return 0
+
+        while self.iidx < self.in_len:
+            ctrl: int = self.in_bytes[self.iidx]
+            self.iidx += 1
+
+            if ctrl < 32:
+                self._copy_byte_sequence(ctrl)
+            else:
+                self._expand_repeated_bytes(ctrl)
+
+        return self.oidx
+
+    def _reset(self):
+        self.iidx = 0
+        self.oidx = 0
+
+    def _copy_byte_sequence(self, ctrl: int):
+        length: int = ctrl + 1
+        if self.oidx + length > len(self.out_bytes):
+            raise AttributeError
+
+        self.out_bytes[self.oidx:self.oidx + length] = self.in_bytes[self.iidx: self.iidx + length]
+        self.oidx += length
+        self.iidx += length
+
+    def _expand_repeated_bytes(self, ctrl: int):
+        length: int = ctrl >> 5
+        reference: int = self.oidx - ((ctrl & 0x1f) << 8) - 1  # magic
+
+        if length == 7:
+            length += self.in_bytes[self.iidx]
+            self.iidx += 1
+
+        if self.oidx + length + 2 > len(self.out_bytes):
+            raise AttributeError
+
+        reference -= self.in_bytes[self.iidx]
+        self.iidx += 1
+
+        reference = self._copy_from_reference_and_increment(reference)
+        reference = self._copy_from_reference_and_increment(reference)
+
+        while length > 0:
+            reference = self._copy_from_reference_and_increment(reference)
+            length -= 1
+
+    def _copy_from_reference_and_increment(self, reference: int) -> int:
+        self.out_bytes[self.oidx] = self.out_bytes[reference]
+        self.oidx += 1
+        return reference + 1
---
+++ tests/test_lzf.py
@@ -0,0 +1,92 @@
+import unittest
+
+from yxdb.lzf import Lzf
+
+
+class TestLzf(unittest.TestCase):
+    def test_empty_input(self):
+        in_bytes = bytearray(0)
+        out_bytes = bytearray(0)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        written = lzf.decompress(0)
+        self.assertEqual(0, written)
+
+    def test_small_control_values_do_simple_copies(self):
+        in_bytes = bytearray([0, 25])
+        out_bytes = bytearray(1)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        written = lzf.decompress(2)
+        self.assertEqual(1, written)
+        self.assertEqual(25, out_bytes[0])
+
+    def test_output_array_is_too_small(self):
+        in_bytes = bytearray([0, 25])
+        out_bytes = bytearray([])
+        lzf = Lzf(in_bytes, out_bytes)
+
+        self.assertRaises(AttributeError, lambda: lzf.decompress(2))
+
+    def test_larger_small_control_value(self):
+        in_bytes = bytearray([4, 1, 2, 3, 4, 5])
+        out_bytes = bytearray(5)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        written = lzf.decompress(6)
+        self.assertEqual(5, written)
+        self.assertEqual(bytearray([1, 2, 3, 4, 5]), out_bytes)
+
+    def test_multiple_small_control_values(self):
+        in_bytes = bytearray([2, 1, 2, 3, 1, 1, 2])
+        out_bytes = bytearray(5)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        written = lzf.decompress(7)
+        self.assertEqual(5, written)
+        self.assertEqual(bytearray([1, 2, 3, 1, 2]), out_bytes)
+
+    def test_expand_large_control_values(self):
+        in_bytes = bytearray([2, 1, 2, 3, 32, 1])
+        out_bytes = bytearray(6)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        written = lzf.decompress(6)
+        self.assertEqual(6, written)
+        self.assertEqual(bytearray([1, 2, 3, 2, 3, 2]), out_bytes)
+
+    def test_large_control_values_with_length_of_7(self):
+        in_bytes = bytearray([8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 224, 1, 8])
+        out_bytes = bytearray(19)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        written = lzf.decompress(13)
+        self.assertEqual(19, written)
+        self.assertEqual(bytearray([1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1]), out_bytes)
+
+    def test_output_array_too_small_for_large_control_value(self):
+        in_bytes = bytearray([8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 224, 1, 8])
+        out_bytes = bytearray(17)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        self.assertRaises(AttributeError, lambda: lzf.decompress(13))
+
+    def test_reset_lzf_and_start_again(self):
+        in_bytes = bytearray([4, 1, 2, 3, 4, 5])
+        out_bytes = bytearray(5)
+        lzf = Lzf(in_bytes, out_bytes)
+
+        lzf.decompress(6)
+
+        in_bytes[0] = 2
+        in_bytes[1] = 6
+        in_bytes[2] = 7
+        in_bytes[3] = 8
+
+        written = lzf.decompress(4)
+        self.assertEqual(3, written)
+        self.assertEqual(bytearray([6, 7, 8, 4, 5]), out_bytes)
+
+
+if __name__ == '__main__':
+    unittest.main()
